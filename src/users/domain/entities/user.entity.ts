30f06381f97a74262fc1be41986844fe2b51e64d
Miqueias Kawa <kawasousa1554@gmail.com>
2023-09-24 18:00:59 -0300

    UserEntity - constructor unit test

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -1,1 +1,12 @@
-export class UserEntity {}
+export type UserProps = {
+  name: string,
+  email: string,
+  password: string,
+  craetedDate?: Date
+}
+
+export class UserEntity {
+  constructor(public readonly props: UserProps){
+    this.props.craetedDate = this.props.craetedDate ?? new Date();
+  }
+}
